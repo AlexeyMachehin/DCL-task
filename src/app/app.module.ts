752a3e8bc098c3cbb1773c3d2de06f61e8307a5e
AlexeyMachehin <Lehamachehin@yandex.ru>
2023-08-08 23:27:07 +0500

    create layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { NgIf } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mockData.ts
@@ -0,0 +1,16 @@
+export const data = {
+  categories: [
+    {
+      categoryName: 'B',
+      cars: [
+        { name: 'BMW X5' },
+        { name: 'Nissan Quashkai ' },
+        { name: 'Ford Focus' },
+      ],
+    },
+    {
+      categoryName: 'C',
+      cars: [{ name: 'Ford Transit' }, { name: 'Mercedes-Benz Sprinter' }],
+    },
+  ],
+};
