859437ff1379a15e03a800e629adb9f4f9f8bb22
AlexeyMachehin <Lehamachehin@yandex.ru>
2023-08-09 04:40:06 +0500

    create pipe

---
+++ src/app/currency-format.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currencyFormat',
+})
+export class CurrencyFormatPipe implements PipeTransform {
+  transform(value: number): string {
+    const formatter = new Intl.NumberFormat('ru-RU', {
+      style: 'currency',
+      currency: 'RUB',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+
+    const formattedValue = formatter.format(value);
+    const currencySymbol = ' ₽';
+
+    return formattedValue
+      .replace(/\s/g, '')
+      .replace('RUB', ' ' + currencySymbol);
+  }
+}
